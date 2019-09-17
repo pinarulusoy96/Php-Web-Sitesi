7eecd86d68c885795616861c97f009e32ea8778c
pinarulusoy96 <55440961+pinarulusoy96@users.noreply.github.com>
2019-09-17 14:46:05 +0300

    Create hakkimizda.php

---
+++ hakkimizda.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="anasayfa.css" />
+    <title>Hakkımızda</title>
+</head>
+<body>
+    <nav>
+        <center>
+        <ul class="fancyNav">
+            <li id="news"><a href="hakkimizda.php">Hakkımızda</a></li>
+            <li id="about"><a href="#about">Konular</a></li>
+            <li id="contact"><a href="iletisim.php">İletişim</a></li>
+        </ul>
+        </center>
+    </nav>
+    <h1>Hakkımızda</h1>
+    <p>178 yıllık köklü bir geçmişe sahip olan Türk Telekom, Türkiye’nin ilk entegre telekomünikasyon operatörüdür. Müşterilerin hızla değişen iletişim ve teknoloji ihtiyaçlarına en güçlü ve en doğru şekilde cevap verebilmek amacıyla 2015 yılında Türk Telekomünikasyon A.Ş., TT Mobil İletişim Hizmetleri A.Ş. ve TTNET A.Ş. tüzel kişiliklerini mevcut şekliyle muhafaza ederek ve tabi oldukları mevzuat ve regülasyonlara tamamen uyarak, “müşteri odaklı” ve entegre bir yapıya geçmiştir. Bireysel ve kurumsal hizmetler alanında geniş hizmet ağı ve zengin ürün çeşitliliğine sahip olan Türk Telekom, Ocak 2016 itibarıyla mobil, internet, telefon ve TV ürün ve hizmetlerini ‘Türk Telekom’ tek marka çatısı altında bir araya getirmiştir.
+        Türk Telekom, 30 Haziran 2019 itibarıyla 14,5 milyon sabit erişim hattı, 11,1 milyon genişbant, 3,6 milyon TV ve 22,4 milyon mobil aboneye hizmet vermektedir. Türk Telekom Grubu şirketleri Türkiye’yi yeni teknolojilerle buluşturma ve bilgi toplumuna dönüşüm sürecini hızlandırma vizyonuyla, 81 ilde 31.467 çalışanıyla hizmet vermektedir.
+        Türk Telekomünikasyon A.Ş., PSTN ve toptan genişbant hizmetlerini sunmakta olup, mobil operatör TT Mobil İletişim Hizmetleri A.Ş., perakende internet hizmeti, IPTV, Uydu TV, Web TV, Mobile TV, Smart TV Hizmetleri sağlayıcısı TTNET A.Ş., yakınsama teknolojileri şirketi Argela Yazılım ve Bilişim Teknolojileri A.Ş., BT çözüm sağlayıcısı Innova Bilişim Çözümleri A.Ş., çevrimiçi eğitim yazılımları şirketi Sebit Eğitim ve Bilgi Teknolojileri A.Ş., çağrı merkezi şirketi AssisTT Rehberlik ve Müşteri Hizmetleri A.Ş., proje geliştirme ve kurumsal risk sermayesi şirketi TT Ventures Proje Geliştirme A.Ş., elektrik tedarik ve satış şirketi TTES Elektrik Tedarik Satış A.Ş.’nin, toptan veri ve kapasite servis sağlayıcısı Türk Telekom International’ın doğrudan; Türk Telekom International iştiraklerinin, Televizyon yayıncılığı ile isteğe bağlı yayıncılık (VOD) hizmetleri sağlayıcısı Net Ekran şirketlerinin, cihaz satış şirketi TT Satış ve Dağıtım Hizmetleri A.Ş.’nin ve ödeme hizmetleri şirketi TT Ödeme Hizmetleri A.Ş.’nin ve rehberlik
+        hizmetleri şirketi 11818 Rehberlik ve Müşteri Hizmetleri A.Ş.’nin ise dolaylı olarak %100’üne sahiptir.
+    </p>
+</body>
+</html>
